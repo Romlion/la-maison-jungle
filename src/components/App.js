0eccbc29fe36b511014f85af229c8f251fdacb4f
Romain <98345704+Romlion@users.noreply.github.com>
2024-03-05 15:35:40 +0100

    avec les différente plantes c'est mieux

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import Banner from "./Banner";
 import logo from '../assets/logo.png'
-// import Cart from "./Cart";
+import Cart from "./Cart";
 import Footer from "./Footer";
 import ShoppingList from "./ShoppingList";
+import '../styles/Layout.css'
 
 function App() {
   return(
     <div>
     <Banner>
-      <img src={logo} alt='La maison jungle' className='lmj-logo'/>
+      <img src={logo} alt='La maison jungle' className='lmj-logo' />
       <h1 className='lmj-title'>La maison jungle</h1>
     </Banner>
-    {/* <Cart/> */}
-    <ShoppingList/>
-    <Footer/>
+    <div className='lmj-layout-inner'>
+      <Cart />
+      <ShoppingList />
     </div>
+    <Footer />
+  </div>
   ) 
 }
 
